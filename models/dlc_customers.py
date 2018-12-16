46deb7949e4eb2b1543851bfadf7508d12c66f71
dunmi9791 <dominic.anyanna@gmail.com>
2018-12-16 11:44:50 +0100

    add menus

--- models/dlc_customers.py
+++ models/dlc_customers.py
@@ -5,9 +5,3 @@ from odoo import models, fields, api
 
 class DlcCustomers(models.Model):
     _name = 'dlc.customers'
-
-    name = fields.Char()
-    first_name = fields.Char()
-    last_name = fields.Char()
-    date_of_birth = fields.Date()
-    expiry_date = fields.Date()
