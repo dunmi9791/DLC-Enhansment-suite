77aba0891563cb80f8e338f18127a082fb8437ad
dunmi9791 <dominic.anyanna@gmail.com>
2018-12-16 20:21:03 +0100

    add menus

--- __manifest__.py
+++ __manifest__.py
@@ -15,7 +15,7 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'tools',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
@@ -23,7 +23,7 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
         'views/actions.xml',
