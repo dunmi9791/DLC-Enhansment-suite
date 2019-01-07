2baa9f706e3afaeb38016e5ef9da81469a8621a2
dunmi9791 <dominic.anyanna@gmail.com>
2019-01-07 07:23:55 +0100

    create relation to status

--- models/models.py
+++ models/models.py
@@ -54,8 +54,8 @@ class Issues(models.Model):
     def create(self, values):
         valuep = {}
         valuep['status'] = values['dlc_status']
-        prd = self.env['dlc.workstation'].create(valuep)
-        values['dlc_id'] = prd.id
+        prd = self.env['dlc.workstation'].search([('id','=',self.dlc_id.id)])
+        prd.write(valuep)
         new_record = super(Issues, self).create(values)
 
         # Add code here
