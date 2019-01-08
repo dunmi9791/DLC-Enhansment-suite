caea25602d837bbe71fd10e40a53f1e0713e4078
dunmi9791 <dominic.anyanna@gmail.com>
2019-01-08 12:35:13 +0100

    add customer details

--- models/dlc_customers.py
+++ models/dlc_customers.py
@@ -8,3 +8,7 @@ class DlcCustomers(models.Model):
     _description = 'dlc customers'
 
     name = fields.Char("Name")
+    dob = fields.Date(string="Date of Birth", required=False, )
+    expiry_date = fields.Date(string="Expiry Date", required=False, )
+    mobile_number = fields.Char(string="Mobile Number", required=False, )
+    email = fields.Char(string="Email", required=False, )
