df64f503755bf099012650a0d08e7796aaba5102
dunmi9791 <dominic.anyanna@gmail.com>
2019-05-20 23:24:12 +0100

    added ddl type card

--- models/production.py
+++ models/production.py
@@ -11,12 +11,13 @@ class Production(models.Model):
     name = fields.Char()
     workstation_id = fields.Many2one(comodel_name="dlc.workstation", string="Office Location", required=False, )
     date = fields.Date(string="Date", required=False, )
-    card_type = fields.Selection(string="Card Type", selection=[('Temporary', 'Temporary'), ('Office Total', 'Office Total'), ], required=False, )
+    card_type = fields.Selection(string="Card Type", selection=[('Temporary', 'Temporary'), ('Office Total', 'Office Total'), ('Permanent', 'Permanent') ], required=False, )
     cdl = fields.Integer(string="CDL", required=False, )
     gdl = fields.Integer(string="GDL", required=False, )
     msl = fields.Integer(string="MSL", required=False, )
     pdl = fields.Integer(string="PDL", required=False, )
     sdl = fields.Integer(string="SDL", required=False, )
+    ddl = fields.Integer(string="DDL", required=False, )
     total = fields.Integer(string="TOTAL", required=False, )
 
 
